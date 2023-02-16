cb1d65bede09e9c93226e398fec13b20f1fab572
Sackitey68 <sackitey68@gmail.com>
2023-02-16 09:13:11 -0800

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - A simple program that uses put() to print an output
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void){
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
