13f39c4480043e85bdd1f520380b871837ff9f3e
Sackitey68 <sackitey68@gmail.com>
2023-02-21 17:57:19 -0800

    Task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,44 +7,44 @@ void print_times_table(int n)
 {
 int x, y, z, k, t;
 
-	for (x = 0; x <= n; x++)
-	{
-		for (y = 0; y <= n; y++)
-		{
-			z = x * y;
-			if (z == 0 && y == 0)
-				_putchar(z + '0');
-			else if (z > 99)
-			{
-				k = z % 10;
-				t = (z / 10) % 10;
-				z /= 100;
-				_putchar(',');
-				_putchar(' ');
-				_putchar(z + '0');
-				_putchar(t + '0');
-				_putchar(k + '0');
-			}
-			else if (z > 9)
-			{
-				k = z % 10;
-				z /= 10;
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(z + '0');
-				_putchar(k + '0');
+for (x = 0; x <= n; x++)	
+{
+for (y = 0; y <= n; y++)
+{
+z = x * y;
+if (z == 0 && y == 0)
+_putchar(z + '0');
+else if (z > 99)
+{			
+k = z % 10;
+t = (z / 10) % 10;
+z /= 100;
+_putchar(',');
+_putchar(' ');
+_putchar(z + '0');
+_putchar(t + '0');
+_putchar(k + '0');
+}
+else if (z > 9)
+{
+k = z % 10;
+z /= 10;
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar(z + '0');
+_putchar(k + '0');
 
-			}
-			else
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(z + '0');
-			}
-		}
-		_putchar('\n');
-	}
+}
+else
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar(' ');
+_putchar(z + '0');
+}
+}
+_putchar('\n');
+}
 }
