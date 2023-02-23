121df5ad91ecc9f30dfebdbf106fc9c67755d9de
Sackitey68 <sackitey68@gmail.com>
2023-02-23 05:26:21 -0800

    Task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_line - print a straight line using putchar of n size
+ * @n: size of line
+ */
+
+void print_line(int n)
+{
+for (; n > 0; n--)
+_putchar('_');
+_putchar('\n');
+}
