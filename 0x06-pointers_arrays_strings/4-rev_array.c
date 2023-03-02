4f412b74ee08022be1b1a8a8f2ddaf8f91f28cca
Sackitey68 <sackitey68@gmail.com>
2023-03-02 07:57:19 -0800

    Task 4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverses the content of an array of integers
+ * @a: int array to reverse
+ * @n: number of elements in the array
+ * Return: concatenated string
+ */
+
+void reverse_array(int *a, int n)
+{
+	int *begin = a;
+	int *end;
+	int hold = 0;
+
+	end = a + n - 1;
+	for (; begin < end; begin++, end--)
+	{
+		hold = *end;
+		*end = *begin;
+		*begin = hold;
+	}
+}
