a4b152d4c5dd40c0900bfac5ac564d202328a5fd
Sackitey68 <sackitey68@gmail.com>
2023-03-13 09:36:07 -0700

    Task 5

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,54 +1,49 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 /**
- * _strlen - get length
- * @c: string
- * Return: returns length
+ * argstostr - concatenates all the arguments of your program.
+ * @ac: arguments count
+ * @av: arguments vector
+ *
+ * Return: a pointer to a new string, or NULL if it fails
  */
-
-int _strlen(char *c)
-{
-	int len;
-
-	for (len = 0; c[len] != '\0'; len++)
-		;
-
-	return (len);
-}
-
-/**
- * argstostr - concat str
- * @ac: number of args
- * @av: args
- * Return: returns a pointer to the two strings concatenated, or NULL if fails
- */
-
 char *argstostr(int ac, char **av)
 {
-	char *ptr;
-	int x, y, len = 0, c;
+	char *str, *s;
+	int i, j, k, len = 0;
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
 
-	for (x = 0; x < ac; x++)
-		len += _strlen(av[x]);
+	for (i = 0; i < ac; i++)
+	{
+		s = av[i];
+		j = 0;
 
-	ptr = malloc((len + ac + 1) * sizeof(char));
+		while (s[j++])
+			len++;
+		len++;
+	}
 
-	if (!ptr)
+	str = (char *)malloc(sizeof(char) * (len + 1));
+	if (str == NULL)
 		return (NULL);
 
-	for (x = 0; x < ac; x++)
+	for (i = 0, j = 0; i < ac && j < len; i++)
 	{
-		for (y = 0; av[x][y] != '\0'; y++, c++)
-			ptr[c] = av[x][y];
-		ptr[c] = '\n';
-		c++;
+		s = av[i];
+		k = 0;
+
+		while (s[k])
+		{
+			str[j] = s[k];
+			k++;
+			j++;
+		}
+		str[j++] = '\n';
 	}
-	ptr[c] = '\0';
-	return (ptr);
+	str[j] = '\0';
+
+	return (str);
 }
