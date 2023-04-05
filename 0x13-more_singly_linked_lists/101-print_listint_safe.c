e32bb2a6bc7a5d18143f410ffa6724bf55a52dd2
Sackitey68 <sackitey68@gmail.com>
2023-04-04 19:25:37 -0700

    Task 12

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,47 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * print_listint_safe - function that prints a listint_t linked list
+ * @head: pointer to the beginning of linked list
+ * Return: the number of nodes in the list
+ */
+
+size_t print_listint_safe(const listint_t *head)
+{
+	int node, flag = 0;
+	listint_t *slow, *fast;
+
+	if (!head)
+		exit(98);
+	for (node = 1; (*head).next && !flag; head = (*head).next, node++)
+	{
+		if ((*head).next)
+			slow = (*head).next;
+		if ((*head).next->next)
+			fast = (*head).next->next;
+		while (slow != fast)
+		{
+			if (slow)
+				slow = (*slow).next;
+			if (fast == head)
+				flag = 1;
+			if (fast && !flag)
+				fast = (*fast).next;
+			if (fast == head)
+				flag = 1;
+			if (fast && !flag)
+				fast = (*fast).next;
+			if (fast == head)
+				flag = 1;
+		}
+		printf("[%p] %d\n", (void *)head, (*head).n);
+	}
+
+	for (; flag && (*head).next != fast; node++, head = (*head).next)
+		printf("[%p] %d\n", (void *)head, (*head).n);
+	printf("[%p] %d\n", (void *)head, (*head).n);
+	if (fast)
+		printf("-> [%p] %d\n", (void *)fast, (*fast).n);
+	return (node);
+}
