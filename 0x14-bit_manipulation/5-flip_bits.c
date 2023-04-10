24ba53ac9b9583905c9e418c3fec06c64d07aeb2
Sackitey68 <sackitey68@gmail.com>
2023-04-10 10:38:56 -0700

    Task 5

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * flip_bits - calls count_bits function
+ * @n: first input number
+ * @m: second input number
+ * Return: number of bits flipped
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int hold = n ^ m, count = 0;
+
+	while (hold)
+	{
+		count = count + hold & 1;
+		hold  >>= 1;
+	}
+	return (count);
+}
