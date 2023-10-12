d3c84b08bddf8ff70946f77372b23dcfd155cb7a
root <sackitey68@gmail.com>
2023-10-11 18:42:28 -0700

    Advanced Task 3

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -19,14 +19,12 @@ int interpolation_search(int *array, size_t size, int value)
 
 	if (array != NULL)
 	{
-		while ((array[high] != array[low]) &&
-		       (value >= array[low]) &&
-		       (value <= array[high]))
+		while ((array[high] != array[low]) && (value >= array[low])
+			&& (value <= array[high]))
 		{
 			pos = low + (((double)(high - low) / (array[high] - array[low])) *
 				     (value - array[low]));
 			printf("Value checked array[%lu] = [%d]\n", pos, array[pos]);
-			
 			if (array[pos] < value)
 				low = pos + 1;
 			else if (value < array[pos])
